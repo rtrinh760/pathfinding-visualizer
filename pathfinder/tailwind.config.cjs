7c200b5e4d0b9c8db9650e14f38f496d8b0f4298
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2022-12-31 18:38:22 -0600

    Add TailwindCSS

---
+++ pathfinder/tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/components/Pathfinder.tsx", "./src/components/Cell.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
