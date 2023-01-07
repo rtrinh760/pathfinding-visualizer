63abbd47dc4cc3b8251951851416f9dd22c2e0aa
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-01-07 18:18:09 -0500

    Add deployment

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:rt-rocket/pathfinding-visualizer.git main:gh-pages
+
+cd -
