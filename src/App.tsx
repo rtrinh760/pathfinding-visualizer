aebb00ffb9167235f0c005c752c308d4840a29b9
rt-rocket <108962288+rt-rocket@users.noreply.github.com>
2023-11-18 23:56:49 -0500

    Add GitHub corner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { Pathfinder } from './components/Pathfinder'
+import { Pathfinder } from "./components/Pathfinder";
+import GithubCorner from "react-github-corner";
 
 function App() {
   return (
     <div className="App">
+      <GithubCorner href="https://github.com/rtrinh760/pathfinding-visualizer" size="125" />
       <Pathfinder />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
